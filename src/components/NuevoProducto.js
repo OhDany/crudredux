35e42e32ee43619ab65535993ffdda328134a99a
Oh Dany <ohdany@Ohs-iMac.local>
2020-09-23 14:47:10 -0500

    Definiendo el action y utilizando useDispatch

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -8,8 +8,30 @@ import {
   Divider,
   Segment,
 } from 'semantic-ui-react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Actions de Redux
+import { crearNuevoProductoAction } from '../actions/productoActions';
 
 const NuevoProducto = () => {
+  // Utilizar useDispatch y te crea una función
+  const dispatch = useDispatch();
+
+  // Manda llamr el action de productoAction
+  const agregarProducto = () => dispatch(crearNuevoProductoAction());
+
+  // Cuando el usuario haga submit
+  const submitNuevoProducto = (e) => {
+    e.preventDefault();
+
+    // Validar formulario
+
+    // Si no hay errores
+
+    // Crear el nuevo producto
+    agregarProducto();
+  };
+
   return (
     <Grid
       verticalAlign="middle"
@@ -25,7 +47,7 @@ const NuevoProducto = () => {
               <Header.Content>Nuevo producto</Header.Content>
             </Header>
             <Divider />
-            <Form>
+            <Form onSubmit={submitNuevoProducto}>
               <Form.Field>
                 <label>Nombre del producto</label>
                 <input placeholder="Nombre del producto" />
