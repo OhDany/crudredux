ab1a62abfcef3eb53893c8f0522a1315cfb5e2e6
Oh Dany <oh.dany.mx@gmail.com>
2020-09-24 13:53:43 -0500

    Configurar el action para descargar productos

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -2,6 +2,9 @@ import {
   AGREGAR_PRODUCTO,
   AGREGAR_PRODUCTO_EXITO,
   AGREGAR_PRODUCTO_ERROR,
+  COMEMZAR_DESCARGA_PRODUCTOS,
+  DESCARGA_PRODUCTOS_EXITO,
+  DESCARGA_PRODUCTOS_ERROR,
 } from '../types';
 
 // Cada reducer tiene su propio state
@@ -13,6 +16,7 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case COMEMZAR_DESCARGA_PRODUCTOS:
     case AGREGAR_PRODUCTO:
       return {
         ...state,
@@ -30,6 +34,7 @@ export default function (state = initialState, action) {
         loading: false,
         error: action.payload,
       };
+
     default:
       return state;
   }
